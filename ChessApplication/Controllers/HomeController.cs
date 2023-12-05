8d23c4e768d7e55a574f9b4ff19a037bf90cb1d7
Artash001 <artashesarshakyan4@gmail.com>
2023-12-05 16:59:26 +0400

    Added figure color

--- ChessApplication/Controllers/HomeController.cs
+++ ChessApplication/Controllers/HomeController.cs
@@ -3,17 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ChessApplication.Chess;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data.Common;
 
 namespace ChessApplication.Controllers;
 
 public class HomeController : Controller
 {
+    private static string[,] chessboard = new string[8, 8];
 
+    [HttpPost]
+    public IActionResult PlacePiece(string selectedPiece,string selectedCoordinate, string selectedColor)
+    {
+        int.TryParse(selectedCoordinate.Substring(1), out int row);
+        int column = selectedCoordinate[0] - 'A';
+        chessboard[row - 1, column] = selectedPiece;
+        ViewBag.Chessboard = chessboard;
+        ViewBag.Pieces = GetPieces();
+        ViewBag.Color = selectedColor;
+        return View("Index");
+    }
 
     public IActionResult Index()
     {
-        ChessBoard chessBoard = new ChessBoard();
-        ViewBag.Chessboard = chessBoard;
+        ViewBag.ChessBoard = chessboard;
         ViewBag.Pieces = GetPieces();
         return View();
     }
