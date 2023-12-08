b2bc29615a289572760a58a6f2930cc6cc9a94a3
Koryun Avanesyan <koryun.avanesyan@gmail.com>
2023-12-08 19:40:49 +0400

    Front End logic moved to site.js

--- ChessApplication/wwwroot/js/site.js
+++ ChessApplication/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var sourceSquare;
 
-// Write your JavaScript code.
+function drag(ev)
+{
+    // Set the data to be transferred during drag  
+    ev.dataTransfer.setData("text/plain", ev.target.innerText);
+
+    // Set the source square
+    sourceSquare = String.fromCharCode(65 + ev.target.parentElement.cellIndex - 1) + (9 - ev.target.parentElement.parentElement.rowIndex);
+}
+
+function allowDrop(ev)
+{
+    // Prevent the default behavior to allow dropping
+    ev.preventDefault();
+}
+
+function drop(ev)
+{
+    // Prevent the default behavior
+    ev.preventDefault();
+
+    // Set the destination square
+    var destinationSquare = String.fromCharCode(65 + ev.target.cellIndex - 1) + (9 - ev.target.parentElement.rowIndex);
+
+    // Log the source and destination squares
+    console.log("Source Square: " + sourceSquare);
+    console.log("Destination Square: " + destinationSquare);
+
+    // Set the destination coordinate in the hidden input field
+    document.getElementById('destinationCoordinate').value = destinationSquare;
+    document.getElementById('selectedCordinate').value = sourceSquare;
+
+    // Submit the form
+    document.forms[0].submit();
+}
